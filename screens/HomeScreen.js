331b98504291c79fba210ba5af7fc49d0c90931e
guneykilicel <kilicelguney@gmail.com>
2022-10-02 16:19:27 +0300

    navigate with tailwindcss

---
+++ screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { TailwindProvider } from 'tailwindcss-react-native';
+
+function HomeScreen() {
+  return (
+      <View>
+        <Text className="text-red-500">Home Screen merhabaa</Text>
+      </View>
+  );
+}
+export default HomeScreen
